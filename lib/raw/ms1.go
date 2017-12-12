3fac4304d0bbff2958d6c84a555d89358abda68e
Felipe da Veiga Leprevost <felipe@leprevost.com.br>
2017-12-12 11:36:07 -0500

    cleaning unused code

--- lib/raw/ms1.go
+++ lib/raw/ms1.go
@@ -12,18 +12,4 @@ type Ms1Scan struct {
 	SpectrumName  string
 	ScanStartTime float64
 	Spectrum      Spectrum
-	//Spectrum      Ms1Spectrum
 }
-
-// // Ms1Spectrum tag
-// type Ms1Spectrum []Ms1Peak
-//
-// // Ms1Peak tag
-// type Ms1Peak struct {
-// 	Mz        float64
-// 	Intensity float64
-// }
-//
-// func (a Ms1Spectrum) Len() int           { return len(a) }
-// func (a Ms1Spectrum) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-// func (a Ms1Spectrum) Less(i, j int) bool { return a[i].Mz < a[j].Mz }
