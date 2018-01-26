ed62a0a738d4375ee59cf92867af796a53caf819
Felipe da Veiga Leprevost <felipe@leprevost.com.br>
2018-01-26 12:49:49 -0500

    fixed pipeline folder lookup

--- cmd/pipeline.go
+++ cmd/pipeline.go
@@ -28,8 +28,14 @@ import (
 // pipelineCmd represents the pipeline command
 var pipelineCmd = &cobra.Command{
 	Use:   "pipeline",
-	Short: "A brief description of your command",
+	Short: "Automatic execution of consecutive analysis steps",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		if len(args) < 1 {
+			logrus.Fatal("You need to provide at least one dataset for the analysis.")
+		} else if len(args) < 2 {
+			logrus.Fatal("You need to provide at least two datasets for the abacus integrative analysis.")
+		}
 
 		paramTemp, _ := sys.GetTemp()
 
@@ -45,6 +51,12 @@ var pipelineCmd = &cobra.Command{
 		}
 
 		file, _ := filepath.Abs(m.Pipeline.Directives)
+
+		// get current directory
+		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		y, e := ioutil.ReadFile(file)
 		if e != nil {
@@ -63,8 +75,8 @@ var pipelineCmd = &cobra.Command{
 			logrus.Info("Executing the pipeline on ", i)
 
 			// getting inside de the dataset folder
-			localDir, _ := filepath.Abs(i)
-			os.Chdir(localDir)
+			dsAbs, _ := filepath.Abs(i)
+			os.Chdir(dsAbs)
 
 			// Workspace
 			wrk.Run(Version, Build, false, false, true)
@@ -81,57 +93,78 @@ var pipelineCmd = &cobra.Command{
 			// Comet
 			if p.Commands.Comet == "yes" {
 				m.Comet = p.Comet
-
 				gobExt := fmt.Sprintf("*.%s", p.Comet.RawExtension)
 				files, e := filepath.Glob(gobExt)
 				if e != nil {
 					logrus.Fatal(e)
 				}
-
 				comet.Run(m, files)
 			}
 
 			// PeptideProphet
 			if p.Commands.PeptideProphet == "yes" {
 				logrus.Info("Executing PeptideProphet")
-
 				m.PeptideProphet = p.PeptideProphet
 				m.PeptideProphet.Output = "interact"
 				m.PeptideProphet.Combine = true
-
 				gobExt := fmt.Sprintf("*.%s", p.PeptideProphet.FileExtension)
 				files, e := filepath.Glob(gobExt)
 				if e != nil {
 					logrus.Fatal(e)
 				}
-
 				peptideprophet.Run(m, files)
 			}
 
 			// ProteinProphet
 			if p.Commands.ProteinProphet == "yes" {
 				logrus.Info("Executing ProteinProphet")
-
 				m.ProteinProphet = p.ProteinProphet
 				m.ProteinProphet.Output = "interact"
-
 				var files []string
 				files = append(files, "interact.pep.xml")
-
 				proteinprophet.Run(m, files)
 			}
+
+			m.Serialize()
+			// return to the top level directory
+			os.Chdir(dir)
+		}
+
+		// Abacus
+		var combinedProtXML string
+		if p.Commands.Abacus == "yes" {
+			logrus.Info("Creating combined protein inference")
+			// return to the top level directory
+			os.Chdir(dir)
+			m.ProteinProphet = p.ProteinProphet
+			m.ProteinProphet.Output = "combined"
+			var files []string
+			for _, j := range args {
+				fqn := fmt.Sprintf("%s%sinteract.pep.xml", j, string(filepath.Separator))
+				fqn, _ = filepath.Abs(fqn)
+				files = append(files, fqn)
+			}
+			proteinprophet.Run(m, files)
+			combinedProtXML = fmt.Sprintf("%s%scombined.prot.xml", dir, string(filepath.Separator))
+		}
+
+		for _, i := range args {
+
+			// getting inside de the dataset folder
+			dsAbs, _ := filepath.Abs(i)
+			os.Chdir(dsAbs)
 
 			// Filter
 			if p.Commands.Filter == "yes" {
 				logrus.Info("Executing filter")
-
 				m.Filter = p.Filter
 				m.Filter.Pex = "interact.pep.xml"
-
 				if p.Commands.ProteinProphet == "yes" {
 					m.Filter.Pox = "interact.prot.xml"
 				}
-
+				if p.Commands.Abacus == "yes" {
+					m.Filter.Pox = combinedProtXML
+				}
 				e := fil.Run(m.Filter)
 				if e != nil {
 					logrus.Fatal(e.Error())
@@ -141,12 +174,9 @@ var pipelineCmd = &cobra.Command{
 			// FreeQuant
 			if p.Commands.FreeQuant == "yes" {
 				logrus.Info("Executing label-free quantification")
-
 				m.Quantify = p.Freequant
-				m.Quantify.Dir = localDir
+				m.Quantify.Dir = dsAbs
 				m.Quantify.Format = "mzML"
-
-				// run label-free quantification
 				e := qua.RunLabelFreeQuantification(m.Quantify)
 				if e != nil {
 					logrus.Fatal(e.Error())
@@ -156,58 +186,40 @@ var pipelineCmd = &cobra.Command{
 			// LabelQuant
 			if p.Commands.LabelQuant == "yes" {
 				logrus.Info("Executing label-based quantification")
-
 				m.Quantify = p.LabelQuant
-				m.Quantify.Dir = localDir
+				m.Quantify.Dir = dsAbs
 				m.Quantify.Format = "mzML"
 				m.Quantify.Brand = "tmt"
-
 				err := qua.RunTMTQuantification(m.Quantify)
 				if err != nil {
 					logrus.Fatal(err)
 				}
 			}
 
+			// Report
 			if p.Commands.Report == "yes" {
 				logrus.Info("Executing report")
-
 				rep.Run(m)
 			}
 
+			// Cluster
 			if p.Commands.Cluster == "yes" {
 				logrus.Info("Executing cluster")
-
 				m.Cluster = p.Cluster
-
 				clu.GenerateReport(m)
 			}
 
 			m.Serialize()
+			// return to the top level directory
+			os.Chdir(dir)
 		}
 
 		// Abacus
 		if p.Commands.Abacus == "yes" {
-
-			if len(args) < 2 {
-				logrus.Fatal("The abacus combined analysis needs at least 2 result files to work")
-			}
-
-			logrus.Info("Creating combined protein inference")
-			m.ProteinProphet = p.ProteinProphet
-			m.ProteinProphet.Output = "combined"
-
-			var files []string
-			for _, j := range args {
-				fqn := fmt.Sprintf("%s%sinteract.pep.xml", j, string(filepath.Separator))
-				fqn, _ = filepath.Abs(fqn)
-				files = append(files, fqn)
-			}
-
-			proteinprophet.Run(m, files)
-
 			logrus.Info("Executing abacus")
+			// return to the top level directory
+			os.Chdir(dir)
 			m.Abacus = p.Abacus
-
 			err := aba.Run(m.Abacus, m.Temp, args)
 			if err != nil {
 				logrus.Fatal(err)
@@ -237,7 +249,6 @@ var pipelineCmd = &cobra.Command{
 		}
 
 		logrus.Info("Done")
-
 		return
 	},
 }
