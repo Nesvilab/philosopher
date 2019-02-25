76c0924914f94d9a46d37dffeec4151819ef169c
Felipe da Veiga Leprevost <felipevl@umich.edu>
2019-02-25 13:07:53 -0500

    fixing slack integration

--- lib/sla/sla.go
+++ lib/sla/sla.go
@@ -1,28 +1,38 @@
 package sla
 
 import (
+	"fmt"
+
 	"github.com/nlopes/slack"
+	"github.com/sirupsen/logrus"
 )
 
 // Run is the main entry point for Slack
 func Run(name, token, message, channel string) {
 
-	//api := slack.New(token)
-	params := slack.PostMessageParameters{}
-	//attachment := slack.Attachment{}
+	api := slack.New(token)
 
-	//params.Attachments = []slack.Attachment{attachment}
-	params.Username = name
-
-	//channelID, timestamp, err := api.PostMessage(channel, message, params)
-
-	// _ = channelID
-	// _ = timestamp
-
-	// if err != nil {
-	// 	fmt.Printf("%s\n", err)
-	// 	return
+	// attachment := slack.Attachment{
+	// 	//Pretext: "some pretext",
+	// 	Text: message,
+	// 	// Uncomment the following part to send a field too
+	// 	/*
+	// 		Fields: []slack.AttachmentField{
+	// 			slack.AttachmentField{
+	// 				Title: "a",
+	// 				Value: "no",
+	// 			},
+	// 		},
+	// 	*/
 	// }
+
+	channelID, timestamp, err := api.PostMessage(channel, slack.MsgOptionText(message, false), slack.MsgOptionUsername(name))
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	logrus.Info("Message successfully sent to channel", channelID, "at", timestamp)
 
 	return
 }
