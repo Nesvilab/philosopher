42907c838b954c4dbe86a36e247d13e4b64da1f7
Felipe da Veiga Leprevost <felipevl@umich.edu>
2019-11-19 13:12:51 -0500

    adding fas tests

--- lib/fas/fas_test.go
+++ lib/fas/fas_test.go
@@ -2,49 +2,58 @@ package fas_test
 
 import (
 	"os"
-
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
 	. "philosopher/lib/fas"
+	"reflect"
+	"testing"
 )
 
-var _ = Describe("Fas", func() {
+// 		It("Parsing UniProt Description", func() {
+// 			f := ParseUniProtDescriptionMap("db/uniprot/2019-02-05-td-hsa-reviewed-2019-02-04.fasta")
+// 			Expect(len(f)).To(Equal(20448))
+// 		})
 
-	Context("Testing database parsing", func() {
+// 		It("Parsing UniProt Sequence", func() {
+// 			f := ParseUniProtSequencenMap("db/uniprot/2019-02-05-td-hsa-reviewed-2019-02-04.fasta")
+// 			Expect(len(f)).To(Equal(20448))
+// 		})
 
-		It("Accessing workspace", func() {
-			e := os.Chdir("../../test/")
-			Expect(e).NotTo(HaveOccurred())
+// 		It("Parsing FASTA Description", func() {
+// 			f := ParseFastaDescription("db/uniprot/2019-02-05-td-hsa-reviewed-2019-02-04.fasta")
+// 			Expect(len(f)).To(Equal(20448))
+// 		})
+
+// 		It("Parsing FASTA File", func() {
+// 			f := ParseFile("db/uniprot/2019-02-05-td-hsa-reviewed-2019-02-04.fasta")
+
+// 			f = CleanDatabase(f, "rev_", "cont_")
+// 			Expect(len(f)).To(Equal(20448))
+// 		})
+
+// 	})
+
+// })
+
+func TestParseFile(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Testing Fasta file parsing",
+			args: args{filename: "db/uniprot/2019-02-05-td-hsa-reviewed-2019-02-04.fasta"},
+			want: 40896,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Chdir("../../test/")
+			if got := ParseFile(tt.args.filename); !reflect.DeepEqual(len(got), tt.want) {
+				t.Errorf("ParseFile() = %d, want %d", len(got), tt.want)
+			}
 		})
-
-		It("Parsing FASTA", func() {
-			f := ParseFile("db/uniprot/2019-02-05-td-hsa-reviewed-2019-02-04.fasta")
-			Expect(len(f)).To(Equal(40896))
-		})
-
-		It("Parsing UniProt Description", func() {
-			f := ParseUniProtDescriptionMap("db/uniprot/2019-02-05-td-hsa-reviewed-2019-02-04.fasta")
-			Expect(len(f)).To(Equal(20448))
-		})
-
-		It("Parsing UniProt Sequence", func() {
-			f := ParseUniProtSequencenMap("db/uniprot/2019-02-05-td-hsa-reviewed-2019-02-04.fasta")
-			Expect(len(f)).To(Equal(20448))
-		})
-
-		It("Parsing FASTA Description", func() {
-			f := ParseFastaDescription("db/uniprot/2019-02-05-td-hsa-reviewed-2019-02-04.fasta")
-			Expect(len(f)).To(Equal(20448))
-		})
-
-		It("Parsing FASTA File", func() {
-			f := ParseFile("db/uniprot/2019-02-05-td-hsa-reviewed-2019-02-04.fasta")
-
-			f = CleanDatabase(f, "rev_", "cont_")
-			Expect(len(f)).To(Equal(20448))
-		})
-
-	})
-
-})
+	}
+}
