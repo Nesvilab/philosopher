cbfce8c29bb515e86cdfaaaf431ae89fbe3f4921
Felipe da Veiga Leprevost <felipevl@umich.edu>
2021-12-03 16:06:51 -0500

    adding protein desc parsing

--- lib/rep/protein.go
+++ lib/rep/protein.go
@@ -187,7 +187,10 @@ func (evi *Evidence) AssembleProteinReport(pro id.ProtIDList, weight float64, de
 	for i := range list {
 		for _, j := range dtb.Records {
 
-			if strings.Contains(j.OriginalHeader, list[i].ProteinName) && strings.Contains(j.OriginalHeader, list[i].Description) {
+			desc := strings.Replace(j.Description, "|", " ", -1)
+
+			if strings.Contains(j.OriginalHeader, list[i].ProteinName) && strings.EqualFold(list[i].Description, desc) {
+				//if strings.Contains(j.OriginalHeader, list[i].ProteinName) && strings.Contains(j.OriginalHeader, list[i].Description) {
 
 				if (j.IsDecoy && list[i].IsDecoy) || (!j.IsDecoy && !list[i].IsDecoy) {
 
