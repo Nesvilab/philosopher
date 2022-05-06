920367e0f8bbda50f03398c557c99f70d11b335d
Dmitry Avtonomov <dmt.avt@gmail.com>
2022-05-06 11:47:16 -0700

    Add basic init script to initialize Assets
    Probably would be best to add cleanup before unzipping.

---
+++ init-build.sh
@@ -0,0 +1,25 @@
+unzip -o lib/dat/bindata.go.zip -d  lib/dat/
+
+unzip -o lib/ext/cdhit/unix/bindata.go.zip -d  lib/ext/cdhit/unix/
+unzip -o lib/ext/cdhit/win/bindata.go.zip -d  lib/ext/cdhit/win/
+
+unzip -o lib/ext/comet/unix/bindata.go.zip -d  lib/ext/comet/unix/
+unzip -o lib/ext/comet/win/bindata.go.zip -d  lib/ext/comet/win/
+
+unzip -o lib/ext/interprophet/unix/bindata.go.zip -d  lib/ext/interprophet/unix/
+unzip -o lib/ext/interprophet/win/bindata.go.zip -d  lib/ext/interprophet/win/
+
+unzip -o lib/ext/peptideprophet/unix/bindata.go.zip -d  lib/ext/peptideprophet/unix/
+unzip -o lib/ext/peptideprophet/win/bindata.go.zip -d  lib/ext/peptideprophet/win/
+
+unzip -o lib/ext/ptmprophet/unix/bindata.go.zip -d  lib/ext/ptmprophet/unix/
+unzip -o lib/ext/ptmprophet/win/bindata.go.zip -d  lib/ext/ptmprophet/win/
+
+unzip -o lib/ext/proteinprophet/unix/bindata.go.zip -d  lib/ext/proteinprophet/unix/
+unzip -o lib/ext/proteinprophet/win/bindata.go.zip -d  lib/ext/proteinprophet/win/
+
+unzip -o lib/pip/bindata.go.zip -d  lib/pip/
+
+unzip -o lib/dat/bindata.go.zip -d  lib/dat/
+
+unzip -o lib/obo/unimod/bindata.go.zip -d  lib/obo/unimod/
