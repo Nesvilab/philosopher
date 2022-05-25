8f09d261f40016d3719eb28549d49d99cc06ec97
Felipe da Veiga Leprevost <felipevl@med.umich.edu>
2022-05-25 14:13:11 -0400

    updating db tests

--- lib/dat/dat_test.go
+++ lib/dat/dat_test.go
@@ -74,7 +74,7 @@ func TestBase_ProcessDB(t *testing.T) {
 			d.ProcessDB(tt.args.file, tt.args.decoyTag)
 
 			if len(d.Records) != 20361 {
-				t.Errorf("Number of FASTA entries is incorrect, got %d, want %d", len(d.Records), 20360)
+				t.Errorf("Number of FASTA entries is incorrect, got %d, want %d", len(d.Records), 20371)
 			}
 		})
 	}
