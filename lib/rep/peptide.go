a9d2c45ebd5419224a500fc69acec5a765af7bec
Felipe da Veiga Leprevost <felipevl@med.umich.edu>
2022-07-27 12:18:01 -0400

    setting proper custom labels on report

--- lib/rep/peptide.go
+++ lib/rep/peptide.go
@@ -155,80 +155,136 @@ func (evi PeptideEvidenceList) MetaPeptideReport(workspace, brand, decoyTag stri
 	if brand == "tmt" {
 		switch channels {
 		case 6:
-			header += "\tChannel 126\tChannel 127N\tChannel 128C\tChannel 129N\tChannel 130C\tChannel 131"
+			header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+				header,
+				printSet[0].Labels.Channel1.CustomName,
+				printSet[0].Labels.Channel2.CustomName,
+				printSet[0].Labels.Channel5.CustomName,
+				printSet[0].Labels.Channel6.CustomName,
+				printSet[0].Labels.Channel9.CustomName,
+				printSet[0].Labels.Channel10.CustomName,
+			)
 		case 10:
-			header += "\tChannel 126\tChannel 127N\tChannel 127C\tChannel 128N\tChannel 128C\tChannel 129N\tChannel 129C\tChannel 130N\tChannel 130C\tChannel 131N"
+			header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+				header,
+				printSet[0].Labels.Channel1.CustomName,
+				printSet[0].Labels.Channel2.CustomName,
+				printSet[0].Labels.Channel3.CustomName,
+				printSet[0].Labels.Channel4.CustomName,
+				printSet[0].Labels.Channel5.CustomName,
+				printSet[0].Labels.Channel6.CustomName,
+				printSet[0].Labels.Channel7.CustomName,
+				printSet[0].Labels.Channel8.CustomName,
+				printSet[0].Labels.Channel9.CustomName,
+				printSet[0].Labels.Channel10.CustomName,
+			)
 		case 11:
-			header += "\tChannel 126\tChannel 127N\tChannel 127C\tChannel 128N\tChannel 128C\tChannel 129N\tChannel 129C\tChannel 130N\tChannel 130C\tChannel 131N\tChannel 131C"
+			header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+				header,
+				printSet[0].Labels.Channel1.CustomName,
+				printSet[0].Labels.Channel2.CustomName,
+				printSet[0].Labels.Channel3.CustomName,
+				printSet[0].Labels.Channel4.CustomName,
+				printSet[0].Labels.Channel5.CustomName,
+				printSet[0].Labels.Channel6.CustomName,
+				printSet[0].Labels.Channel7.CustomName,
+				printSet[0].Labels.Channel8.CustomName,
+				printSet[0].Labels.Channel9.CustomName,
+				printSet[0].Labels.Channel10.CustomName,
+				printSet[0].Labels.Channel11.CustomName,
+			)
 		case 16:
-			header += "\tChannel 126\tChannel 127N\tChannel 127C\tChannel 128N\tChannel 128C\tChannel 129N\tChannel 129C\tChannel 130N\tChannel 130C\tChannel 131N\tChannel 131C\tChannel 132N\tChannel 132C\tChannel 133N\tChannel 133C\tChannel 134N"
+			header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+				header,
+				printSet[0].Labels.Channel1.CustomName,
+				printSet[0].Labels.Channel2.CustomName,
+				printSet[0].Labels.Channel3.CustomName,
+				printSet[0].Labels.Channel4.CustomName,
+				printSet[0].Labels.Channel5.CustomName,
+				printSet[0].Labels.Channel6.CustomName,
+				printSet[0].Labels.Channel7.CustomName,
+				printSet[0].Labels.Channel8.CustomName,
+				printSet[0].Labels.Channel9.CustomName,
+				printSet[0].Labels.Channel10.CustomName,
+				printSet[0].Labels.Channel11.CustomName,
+				printSet[0].Labels.Channel12.CustomName,
+				printSet[0].Labels.Channel13.CustomName,
+				printSet[0].Labels.Channel14.CustomName,
+				printSet[0].Labels.Channel15.CustomName,
+				printSet[0].Labels.Channel16.CustomName,
+			)
 		case 18:
-			header += "\tChannel 126\tChannel 127N\tChannel 127C\tChannel 128N\tChannel 128C\tChannel 129N\tChannel 129C\tChannel 130N\tChannel 130C\tChannel 131N\tChannel 131C\tChannel 132N\tChannel 132C\tChannel 133N\tChannel 133C\tChannel 134N\tChannel 134C\tChannel 135N"
+			header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+				header,
+				printSet[0].Labels.Channel1.CustomName,
+				printSet[0].Labels.Channel2.CustomName,
+				printSet[0].Labels.Channel3.CustomName,
+				printSet[0].Labels.Channel4.CustomName,
+				printSet[0].Labels.Channel5.CustomName,
+				printSet[0].Labels.Channel6.CustomName,
+				printSet[0].Labels.Channel7.CustomName,
+				printSet[0].Labels.Channel8.CustomName,
+				printSet[0].Labels.Channel9.CustomName,
+				printSet[0].Labels.Channel10.CustomName,
+				printSet[0].Labels.Channel11.CustomName,
+				printSet[0].Labels.Channel12.CustomName,
+				printSet[0].Labels.Channel13.CustomName,
+				printSet[0].Labels.Channel14.CustomName,
+				printSet[0].Labels.Channel15.CustomName,
+				printSet[0].Labels.Channel16.CustomName,
+				printSet[0].Labels.Channel17.CustomName,
+				printSet[0].Labels.Channel18.CustomName,
+			)
 		default:
 			header += ""
 		}
 	} else if brand == "itraq" {
 		switch channels {
 		case 4:
-			header += "\tChannel 114\tChannel 115\tChannel 116\tChannel 117"
+			header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\n",
+				header,
+				printSet[0].Labels.Channel1.CustomName,
+				printSet[0].Labels.Channel2.CustomName,
+				printSet[0].Labels.Channel3.CustomName,
+				printSet[0].Labels.Channel4.CustomName,
+			)
 		case 8:
-			header += "\tChannel 113\tChannel 114\tChannel 115\tChannel 116\tChannel 117\tChannel 118\tChannel 119\tChannel 121"
+			header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+				header,
+				printSet[0].Labels.Channel1.CustomName,
+				printSet[0].Labels.Channel2.CustomName,
+				printSet[0].Labels.Channel3.CustomName,
+				printSet[0].Labels.Channel4.CustomName,
+				printSet[0].Labels.Channel5.CustomName,
+				printSet[0].Labels.Channel6.CustomName,
+				printSet[0].Labels.Channel7.CustomName,
+				printSet[0].Labels.Channel8.CustomName,
+			)
 		default:
 			header += ""
 		}
 	} else if brand == "xtag" {
-		header += "\tChannel xTag1\tChannel xTag2\tChannel xTag3\tChannel xTag4\tChannel xTag5\tChannel xTag6\tChannel xTag7\tChannel xTag8\tChannel xTag9\tChannel xTag10\tChannel xTag11\tChannel xTag12\tChannel xTag13\tChannel xTag14\tChannel xTag15\tChannel xTag16\tChannel xTag17\tChannel xTag18"
-	}
-
-	header += "\n"
-
-	// verify if the structure has labels, if so, replace the original channel names by them.
-	if hasLabels {
-
-		var c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18 string
-
-		for _, i := range printSet {
-			if len(i.Labels.Channel1.CustomName) >= 1 {
-				c1 = i.Labels.Channel1.CustomName
-				c2 = i.Labels.Channel2.CustomName
-				c3 = i.Labels.Channel3.CustomName
-				c4 = i.Labels.Channel4.CustomName
-				c5 = i.Labels.Channel5.CustomName
-				c6 = i.Labels.Channel6.CustomName
-				c7 = i.Labels.Channel7.CustomName
-				c8 = i.Labels.Channel8.CustomName
-				c9 = i.Labels.Channel9.CustomName
-				c10 = i.Labels.Channel10.CustomName
-				c11 = i.Labels.Channel11.CustomName
-				c12 = i.Labels.Channel12.CustomName
-				c13 = i.Labels.Channel13.CustomName
-				c14 = i.Labels.Channel14.CustomName
-				c15 = i.Labels.Channel15.CustomName
-				c16 = i.Labels.Channel16.CustomName
-				c17 = i.Labels.Channel17.CustomName
-				c18 = i.Labels.Channel18.CustomName
-				break
-			}
-		}
-
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel1.Name, c1, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel2.Name, c2, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel3.Name, c3, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel4.Name, c4, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel5.Name, c5, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel6.Name, c6, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel7.Name, c7, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel8.Name, c8, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel9.Name, c9, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel10.Name, c10, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel11.Name, c11, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel12.Name, c12, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel13.Name, c13, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel14.Name, c14, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel15.Name, c15, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel16.Name, c16, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel17.Name, c17, -1)
-		header = strings.Replace(header, "Channel "+printSet[10].Labels.Channel18.Name, c18, -1)
+		header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+			header,
+			printSet[0].Labels.Channel1.CustomName,
+			printSet[0].Labels.Channel2.CustomName,
+			printSet[0].Labels.Channel3.CustomName,
+			printSet[0].Labels.Channel4.CustomName,
+			printSet[0].Labels.Channel5.CustomName,
+			printSet[0].Labels.Channel6.CustomName,
+			printSet[0].Labels.Channel7.CustomName,
+			printSet[0].Labels.Channel8.CustomName,
+			printSet[0].Labels.Channel9.CustomName,
+			printSet[0].Labels.Channel10.CustomName,
+			printSet[0].Labels.Channel11.CustomName,
+			printSet[0].Labels.Channel12.CustomName,
+			printSet[0].Labels.Channel13.CustomName,
+			printSet[0].Labels.Channel14.CustomName,
+			printSet[0].Labels.Channel15.CustomName,
+			printSet[0].Labels.Channel16.CustomName,
+			printSet[0].Labels.Channel17.CustomName,
+			printSet[0].Labels.Channel18.CustomName,
+		)
 	}
 
 	//_, e = io.WriteString(file, header)
