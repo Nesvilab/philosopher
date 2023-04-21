Nesvilab/philosopher

commit 0f808986b3849442564c08e5596614eed599dbde

adding support for sCLIP

Makefile
@@ -7,7 +7,7 @@ VERSION = $(shell date +%Y%m%d)
 BUILD = $(shell  date +%Y%m%d%H%M)
 
 TAG = v5.0.0
-RC = RC22
+RC = RC23
 
 LDFLAGS = -ldflags "-w -s -extldflags -static -X main.version=${TAG} -X main.build=${BUILD}"
 

lib/qua/iso.go
@@ -7,14 +7,14 @@ import (
 	"strings"
 
 	"github.com/Nesvilab/philosopher/lib/id"
-	"github.com/Nesvilab/philosopher/lib/xta"
-
 	"github.com/Nesvilab/philosopher/lib/iso"
 	"github.com/Nesvilab/philosopher/lib/msg"
 	"github.com/Nesvilab/philosopher/lib/mzn"
 	"github.com/Nesvilab/philosopher/lib/rep"
+	"github.com/Nesvilab/philosopher/lib/scl"
 	"github.com/Nesvilab/philosopher/lib/tmt"
 	"github.com/Nesvilab/philosopher/lib/trq"
+	"github.com/Nesvilab/philosopher/lib/xta"
 )
 
 const (
@@ -36,6 +36,8 @@ func prepareLabelStructureWithMS2(dir, format, brand, plex string, tol float64, 
 				labelData = tmt.New(plex)
 			} else if brand == "itraq" {
 				labelData = trq.New(plex)
+			} else if brand == "sclip" {
+				labelData = scl.New(plex)
 			} else if brand == "xtag" {
 				labelData = xta.New(plex)
 			}
@@ -157,7 +159,7 @@ func prepareLabelStructureWithMS2(dir, format, brand, plex string, tol float64, 
 					}
 				}
 
-				if brand != "xtag" && i.Mz.DecodedStream[j] > 137 {
+				if brand != "sclip" && i.Mz.DecodedStream[j] > 137 {
 					break
 				} else if i.Mz.DecodedStream[j] > 450 {
 					break
@@ -310,7 +312,7 @@ func prepareLabelStructureWithMS3(dir, format, brand, plex string, tol float64, 
 					}
 				}
 
-				if brand != "xtag" && i.Mz.DecodedStream[j] > 137 {
+				if brand != "sclip" && i.Mz.DecodedStream[j] > 137 {
 					break
 				} else if i.Mz.DecodedStream[j] > 450 {
 					break

cmd/labelquant.go
@@ -68,7 +68,7 @@ func init() {
 		labelquantCmd.Flags().StringVarP(&m.Quantify.Annot, "annot", "", "", "annotation file with custom names for the TMT channels")
 		labelquantCmd.Flags().StringVarP(&m.Quantify.Plex, "plex", "", "", "number of reporter ion channels")
 		labelquantCmd.Flags().StringVarP(&m.Quantify.Dir, "dir", "", "", "folder path containing the raw files")
-		labelquantCmd.Flags().StringVarP(&m.Quantify.Brand, "brand", "", "", "isobaric labeling brand (tmt, itraq)")
+		labelquantCmd.Flags().StringVarP(&m.Quantify.Brand, "brand", "", "", "isobaric labeling brand (tmt, itraq, sCLIP)")
 		labelquantCmd.Flags().Float64VarP(&m.Quantify.Tol, "tol", "", 20, "m/z tolerance in ppm")
 		labelquantCmd.Flags().IntVarP(&m.Quantify.Level, "level", "", 2, "ms level for the quantification")
 		labelquantCmd.Flags().Float64VarP(&m.Quantify.Purity, "purity", "", 0.5, "ion purity threshold")

lib/qua/qua.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/Nesvilab/philosopher/lib/id"
+	"github.com/Nesvilab/philosopher/lib/scl"
 	"github.com/Nesvilab/philosopher/lib/xta"
 
 	"github.com/Nesvilab/philosopher/lib/iso"
@@ -229,6 +230,9 @@ func cleanPreviousData(evi rep.Evidence, brand, plex string) rep.Evidence {
 		} else if brand == "itraq" {
 			evi.PSM[i].Labels = &iso.Labels{}
 			*evi.PSM[i].Labels = trq.New(plex)
+		} else if brand == "sclip" {
+			evi.PSM[i].Labels = &iso.Labels{}
+			*evi.PSM[i].Labels = scl.New(plex)
 		} else if brand == "xtag" {
 			evi.PSM[i].Labels = &iso.Labels{}
 			*evi.PSM[i].Labels = xta.New(plex)
@@ -242,6 +246,9 @@ func cleanPreviousData(evi rep.Evidence, brand, plex string) rep.Evidence {
 		} else if brand == "itraq" {
 			evi.Ions[i].Labels = &iso.Labels{}
 			*evi.Ions[i].Labels = trq.New(plex)
+		} else if brand == "sclip" {
+			evi.Ions[i].Labels = &iso.Labels{}
+			*evi.Ions[i].Labels = scl.New(plex)
 		} else if brand == "xtag" {
 			evi.Ions[i].Labels = &iso.Labels{}
 			*evi.Ions[i].Labels = xta.New(plex)
@@ -263,6 +270,13 @@ func cleanPreviousData(evi rep.Evidence, brand, plex string) rep.Evidence {
 			*evi.Proteins[i].TotalLabels = trq.New(plex)
 			*evi.Proteins[i].UniqueLabels = trq.New(plex)
 			*evi.Proteins[i].URazorLabels = trq.New(plex)
+		} else if brand == "sclip" {
+			evi.Proteins[i].TotalLabels = &iso.Labels{}
+			evi.Proteins[i].UniqueLabels = &iso.Labels{}
+			evi.Proteins[i].URazorLabels = &iso.Labels{}
+			*evi.Proteins[i].TotalLabels = scl.New(plex)
+			*evi.Proteins[i].UniqueLabels = scl.New(plex)
+			*evi.Proteins[i].URazorLabels = scl.New(plex)
 		} else if brand == "xtag" {
 			evi.Proteins[i].TotalLabels = &iso.Labels{}
 			evi.Proteins[i].UniqueLabels = &iso.Labels{}
@@ -465,6 +479,116 @@ func assignLabelNames(labels map[string]iso.Labels, labelNames map[string]string
 				v2.Channel8.CustomName = "121"
 			} else {
 				v2.Channel8.CustomName = labelNames["121"]
+			}
+
+		} else if brand == "sclip" {
+
+			if len(labelNames["sCLIP1"]) < 1 {
+				v2.Channel1.CustomName = "sCLIP1"
+			} else {
+				v2.Channel1.CustomName = labelNames["sCLIP1"]
+			}
+
+			if len(labelNames["sCLIP2"]) < 1 {
+				v2.Channel2.CustomName = "sCLIP2"
+			} else {
+				v2.Channel2.CustomName = labelNames["sCLIP2"]
+			}
+
+			if len(labelNames["sCLIP3"]) < 1 {
+				v2.Channel3.CustomName = "sCLIP3"
+			} else {
+				v2.Channel3.CustomName = labelNames["sCLIP3"]
+			}
+
+			if len(labelNames["sCLIP4"]) < 1 {
+				v2.Channel4.CustomName = "sCLIP4"
+			} else {
+				v2.Channel4.CustomName = labelNames["sCLIP4"]
+			}
+
+			if len(labelNames["sCLIP5"]) < 1 {
+				v2.Channel5.CustomName = "sCLIP5"
+			} else {
+				v2.Channel5.CustomName = labelNames["sCLIP5"]
+			}
+
+			if len(labelNames["sCLIP6"]) < 1 {
+				v2.Channel6.CustomName = "sCLIP6"
+			} else {
+				v2.Channel6.CustomName = labelNames["sCLIP6"]
+			}
+
+			if len(labelNames["sCLIP7"]) < 1 {
+				v2.Channel7.CustomName = "sCLIP7"
+			} else {
+				v2.Channel7.CustomName = labelNames["sCLIP7"]
+			}
+
+			if len(labelNames["sCLIP8"]) < 1 {
+				v2.Channel8.CustomName = "sCLIP8"
+			} else {
+				v2.Channel8.CustomName = labelNames["sCLIP8"]
+			}
+
+			if len(labelNames["sCLIP9"]) < 1 {
+				v2.Channel9.CustomName = "sCLIP9"
+			} else {
+				v2.Channel9.CustomName = labelNames["sCLIP9"]
+			}
+
+			if len(labelNames["sCLIP10"]) < 1 {
+				v2.Channel10.CustomName = "sCLIP10"
+			} else {
+				v2.Channel10.CustomName = labelNames["sCLIP10"]
+			}
+
+			if len(labelNames["sCLIP11"]) < 1 {
+				v2.Channel11.CustomName = "sCLIP11"
+			} else {
+				v2.Channel11.CustomName = labelNames["sCLIP11"]
+			}
+
+			if len(labelNames["sCLIP12"]) < 1 {
+				v2.Channel12.CustomName = "sCLIP12"
+			} else {
+				v2.Channel12.CustomName = labelNames["sCLIP12"]
+			}
+
+			if len(labelNames["sCLIP13"]) < 1 {
+				v2.Channel13.CustomName = "sCLIP13"
+			} else {
+				v2.Channel13.CustomName = labelNames["sCLIP13"]
+			}
+
+			if len(labelNames["sCLIP14"]) < 1 {
+				v2.Channel14.CustomName = "sCLIP14"
+			} else {
+				v2.Channel14.CustomName = labelNames["sCLIP14"]
+			}
+
+			if len(labelNames["sCLIP15"]) < 1 {
+				v2.Channel15.CustomName = "sCLIP15"
+			} else {
+				v2.Channel15.CustomName = labelNames["sCLIP15"]
+			}
+
+			if len(labelNames["sCLIP16"]) < 1 {
+				v2.Channel16.CustomName = "sCLIP16"
+			} else {
+				v2.Channel16.CustomName = labelNames["sCLIP16"]
+			}
+
+			if len(labelNames["sCLIP17"]) < 1 {
+				v2.Channel17.CustomName = "sCLIP17"
+			} else {
+				v2.Channel17.CustomName = labelNames["sCLIP17"]
+			}
+
+			if len(labelNames["sCLIP18"]) < 1 {
+				v2.Channel18.CustomName = "sCLIP18"
+			} else {
+				v2.Channel18.CustomName = labelNames["sCLIP18"]
 			}
 
 		} else if brand == "xtag" {

lib/rep/ion.go
@@ -266,6 +266,28 @@ func (evi IonEvidenceList) IonReport(workspace, brand, decoyTag string, channels
 		default:
 			header += ""
 		}
+	} else if brand == "sclip" {
+		header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s",
+			header,
+			printSet[headerIndex].Labels.Channel1.CustomName,
+			printSet[headerIndex].Labels.Channel2.CustomName,
+			printSet[headerIndex].Labels.Channel3.CustomName,
+			printSet[headerIndex].Labels.Channel4.CustomName,
+			printSet[headerIndex].Labels.Channel5.CustomName,
+			printSet[headerIndex].Labels.Channel6.CustomName,
+			printSet[headerIndex].Labels.Channel7.CustomName,
+			printSet[headerIndex].Labels.Channel8.CustomName,
+			printSet[headerIndex].Labels.Channel9.CustomName,
+			printSet[headerIndex].Labels.Channel10.CustomName,
+			printSet[headerIndex].Labels.Channel11.CustomName,
+			printSet[headerIndex].Labels.Channel12.CustomName,
+			printSet[headerIndex].Labels.Channel13.CustomName,
+			printSet[headerIndex].Labels.Channel14.CustomName,
+			printSet[headerIndex].Labels.Channel15.CustomName,
+			printSet[headerIndex].Labels.Channel16.CustomName,
+			printSet[headerIndex].Labels.Channel17.CustomName,
+			printSet[headerIndex].Labels.Channel18.CustomName,
+		)
 	} else if brand == "xtag" {
 		header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s",
 			header,
@@ -464,6 +486,28 @@ func (evi IonEvidenceList) IonReport(workspace, brand, decoyTag string, channels
 			default:
 				header += ""
 			}
+		} else if brand == "sclip" {
+			line = fmt.Sprintf("%s\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f",
+				line,
+				i.Labels.Channel1.Intensity,
+				i.Labels.Channel2.Intensity,
+				i.Labels.Channel3.Intensity,
+				i.Labels.Channel4.Intensity,
+				i.Labels.Channel5.Intensity,
+				i.Labels.Channel6.Intensity,
+				i.Labels.Channel7.Intensity,
+				i.Labels.Channel8.Intensity,
+				i.Labels.Channel9.Intensity,
+				i.Labels.Channel10.Intensity,
+				i.Labels.Channel11.Intensity,
+				i.Labels.Channel12.Intensity,
+				i.Labels.Channel13.Intensity,
+				i.Labels.Channel14.Intensity,
+				i.Labels.Channel15.Intensity,
+				i.Labels.Channel16.Intensity,
+				i.Labels.Channel17.Intensity,
+				i.Labels.Channel18.Intensity,
+			)
 		} else if brand == "xtag" {
 			line = fmt.Sprintf("%s\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f",
 				line,

lib/rep/msstats.go
@@ -102,6 +102,8 @@ func (evi Evidence) MetaMSstatsReport(workspace, brand string, channels int, has
 		default:
 			header += ""
 		}
+	} else if brand == "sclip" {
+		header += ",Channel sCLIP1,Channel sCLIP2,Channel sCLIP3,Channel sCLIPv4,Channel sCLIP5,Channel sCLIP6,Channel sCLIP7,Channel sCLIP8,Channel sCLIP9,Channel sCLIP10,Channel sCLIP11,Channel sCLIP12,Channel sCLIP13,Channel sCLIP14,Channel sCLIP15,Channel sCLIP16,Channel sCLIP17,Channel sCLIP18"
 	} else if brand == "xtag" {
 		header += ",Channel xTag1,Channel xTag2,Channel xTag3,Channel xTag4,Channel xTag5,Channel xTag6,Channel xTag7,Channel xTag8,Channel xTag9,Channel xTag10,Channel xTag11,Channel xTag12,Channel xTag13,Channel xTag14,Channel xTag15,Channel xTag16,Channel xTag17,Channel xTag18"
 	}

lib/rep/peptide.go
@@ -292,6 +292,28 @@ func (evi PeptideEvidenceList) PeptideReport(workspace, brand, decoyTag string, 
 		default:
 			header += ""
 		}
+	} else if brand == "sclip" {
+		header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s",
+			header,
+			printSet[headerIndex].Labels.Channel1.CustomName,
+			printSet[headerIndex].Labels.Channel2.CustomName,
+			printSet[headerIndex].Labels.Channel3.CustomName,
+			printSet[headerIndex].Labels.Channel4.CustomName,
+			printSet[headerIndex].Labels.Channel5.CustomName,
+			printSet[headerIndex].Labels.Channel6.CustomName,
+			printSet[headerIndex].Labels.Channel7.CustomName,
+			printSet[headerIndex].Labels.Channel8.CustomName,
+			printSet[headerIndex].Labels.Channel9.CustomName,
+			printSet[headerIndex].Labels.Channel10.CustomName,
+			printSet[headerIndex].Labels.Channel11.CustomName,
+			printSet[headerIndex].Labels.Channel12.CustomName,
+			printSet[headerIndex].Labels.Channel13.CustomName,
+			printSet[headerIndex].Labels.Channel14.CustomName,
+			printSet[headerIndex].Labels.Channel15.CustomName,
+			printSet[headerIndex].Labels.Channel16.CustomName,
+			printSet[headerIndex].Labels.Channel17.CustomName,
+			printSet[headerIndex].Labels.Channel18.CustomName,
+		)
 	} else if brand == "xtag" {
 		header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s",
 			header,
@@ -493,6 +515,28 @@ func (evi PeptideEvidenceList) PeptideReport(workspace, brand, decoyTag string, 
 			default:
 				header += ""
 			}
+		} else if brand == "sclip" {
+			line = fmt.Sprintf("%s\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f",
+				line,
+				i.Labels.Channel1.Intensity,
+				i.Labels.Channel2.Intensity,
+				i.Labels.Channel3.Intensity,
+				i.Labels.Channel4.Intensity,
+				i.Labels.Channel5.Intensity,
+				i.Labels.Channel6.Intensity,
+				i.Labels.Channel7.Intensity,
+				i.Labels.Channel8.Intensity,
+				i.Labels.Channel9.Intensity,
+				i.Labels.Channel10.Intensity,
+				i.Labels.Channel11.Intensity,
+				i.Labels.Channel12.Intensity,
+				i.Labels.Channel13.Intensity,
+				i.Labels.Channel14.Intensity,
+				i.Labels.Channel15.Intensity,
+				i.Labels.Channel16.Intensity,
+				i.Labels.Channel17.Intensity,
+				i.Labels.Channel18.Intensity,
+			)
 		} else if brand == "xtag" {
 			line = fmt.Sprintf("%s\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f",
 				line,

lib/rep/protein.go
@@ -399,6 +399,28 @@ func (eviProteins ProteinEvidenceList) ProteinReport(workspace, brand, decoyTag 
 		default:
 			header += ""
 		}
+	} else if brand == "sclip" {
+		header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s",
+			header,
+			printSet[headerIndex].URazorLabels.Channel1.CustomName,
+			printSet[headerIndex].URazorLabels.Channel2.CustomName,
+			printSet[headerIndex].URazorLabels.Channel3.CustomName,
+			printSet[headerIndex].URazorLabels.Channel4.CustomName,
+			printSet[headerIndex].URazorLabels.Channel5.CustomName,
+			printSet[headerIndex].URazorLabels.Channel6.CustomName,
+			printSet[headerIndex].URazorLabels.Channel7.CustomName,
+			printSet[headerIndex].URazorLabels.Channel8.CustomName,
+			printSet[headerIndex].URazorLabels.Channel9.CustomName,
+			printSet[headerIndex].URazorLabels.Channel10.CustomName,
+			printSet[headerIndex].URazorLabels.Channel11.CustomName,
+			printSet[headerIndex].URazorLabels.Channel12.CustomName,
+			printSet[headerIndex].URazorLabels.Channel13.CustomName,
+			printSet[headerIndex].URazorLabels.Channel14.CustomName,
+			printSet[headerIndex].URazorLabels.Channel15.CustomName,
+			printSet[headerIndex].URazorLabels.Channel16.CustomName,
+			printSet[headerIndex].URazorLabels.Channel17.CustomName,
+			printSet[headerIndex].URazorLabels.Channel18.CustomName,
+		)
 	} else if brand == "xtag" {
 		header = fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s",
 			header,
@@ -636,6 +658,28 @@ func (eviProteins ProteinEvidenceList) ProteinReport(workspace, brand, decoyTag 
 			default:
 				header += ""
 			}
+		} else if brand == "sclip" {
+			line = fmt.Sprintf("%s\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f",
+				line,
+				reportIntensities[0],
+				reportIntensities[1],
+				reportIntensities[2],
+				reportIntensities[3],
+				reportIntensities[4],
+				reportIntensities[5],
+				reportIntensities[6],
+				reportIntensities[7],
+				reportIntensities[8],
+				reportIntensities[9],
+				reportIntensities[10],
+				reportIntensities[11],
+				reportIntensities[12],
+				reportIntensities[13],
+				reportIntensities[14],
+				reportIntensities[15],
+				reportIntensities[16],
+				reportIntensities[17],
+			)
 		} else if brand == "xtag" {
 			line = fmt.Sprintf("%s\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f",
 				line,

commit 9f50ec4dad29fde4e60288e64f4923c129524cc1

fix bug introduced in 0f80898 for xtag masses

Makefile
@@ -7,7 +7,7 @@ VERSION = $(shell date +%Y%m%d)
 BUILD = $(shell  date +%Y%m%d%H%M)
 
 TAG = v5.0.0
-RC = RC24
+RC = RC25
 
 LDFLAGS = -ldflags "-w -s -extldflags -static -X main.version=${TAG} -X main.build=${BUILD}"
 

lib/qua/iso.go
@@ -159,7 +159,7 @@ func prepareLabelStructureWithMS2(dir, format, brand, plex string, tol float64, 
 					}
 				}
 
-				if brand != "sclip" && i.Mz.DecodedStream[j] > 137 {
+				if brand != "sclip" && brand != "xtag" && i.Mz.DecodedStream[j] > 137 {
 					break
 				} else if i.Mz.DecodedStream[j] > 450 {
 					break
@@ -312,7 +312,7 @@ func prepareLabelStructureWithMS3(dir, format, brand, plex string, tol float64, 
 					}
 				}
 
-				if brand != "sclip" && i.Mz.DecodedStream[j] > 137 {
+				if brand != "sclip" && brand != "xtag" && i.Mz.DecodedStream[j] > 137 {
 					break
 				} else if i.Mz.DecodedStream[j] > 450 {
 					break